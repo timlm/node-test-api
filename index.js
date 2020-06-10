e3987f4a3737513f58985a34073a26b7dbbf87ab
Tim <minh-tim.le@capgemini.com>
2020-06-10 11:35:28 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, () => {
+    console.info("Server running on port %d", 3000);
+});
+
+app.use(function (req,res,next) {
+    console.log("j'ai été appelé !");
+    next("coucou");
+});
+
+app.use(function(err,req,res,next) {
+    const code = Math.random() < 0.5 ? 502 : 200;
+    console.log(code);
+    res.status(code).json({value:"nope"});
+});
